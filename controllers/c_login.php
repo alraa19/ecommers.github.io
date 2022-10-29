0919c74e2098ef0cc256ebeb9faea2e90398b475
alraa19 <alyarahmauthanty@gmail.com>
2022-10-29 09:41:42 +0700

    commit pertama

---
+++ controllers/c_login.php
@@ -0,0 +1,10 @@
+<?php
+if($aksi=='index'){
+    $data['title']= 'Halaman Login';
+    $helpers->load_view('templates/header.php',$data);
+    $helpers->load_view('login/login.php');
+    $helpers->load_view('templates/footer.php');
+
+}else if($aksi=='Send'){
+    echo"sedang diproses";
+}
